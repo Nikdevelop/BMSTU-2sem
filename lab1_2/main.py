aebff0733044e0faa0feac2b32161fa7a21b90aa
Nikita <zhnikita3@gmail.com>
2024-02-11 14:26:03 +0300

    Added checks and float conversion

--- lab1_2/main.py
+++ lab1_2/main.py
@@ -1,10 +1,11 @@
 from PyQt6.QtWidgets import QApplication, QMessageBox
 from PyQt6 import uic
 import sys
-
 import utils
 
 cur_10_to_9 = True
+PREC = 10
+ALLOWED_SYMBOLS = ".0123456789"
 
 
 def change_mode(window):
@@ -25,13 +26,19 @@ def convert(textbox, main_widget):
     global cur_10_to_9
     number = textbox.text()
     if cur_10_to_9:
-        converted = utils.convert(number, 10, 9)
+        if "." in number:
+            converted = utils.convert_float(number, 10, 9, PREC)
+        else:
+            converted = utils.convert_int(number, 10, 9)
     else:
         if "9" in number:
             QMessageBox.critical(main_widget, "Error", "Incorrect number!")
             textbox.clear()
             return
-        converted = utils.convert(number, 9, 10)
+        if "." in number:
+            converted = utils.convert_float(number, 9, 10, PREC)
+        else:
+            converted = utils.convert_int(number, 9, 10)
 
     textbox.clear()
     textbox.insert(converted)
@@ -40,14 +47,32 @@ def convert(textbox, main_widget):
 def clear_textbox(textbox):
     textbox.clear()
 
+
 def show_info(window):
-    QMessageBox.about(window, "Информация об авторе", "Выполнил Жижин Никита.\nГруппа: ИУ7-21Б")
+    QMessageBox.about(
+        window, "Информация об авторе", "Выполнил Жижин Никита.\nГруппа: ИУ7-21Б"
+    )
+
+
+def check_input(textbox):
+    global cur_10_to_9
+    cur_allowed = ALLOWED_SYMBOLS
+    if not cur_10_to_9:
+        cur_allowed = cur_allowed[:-1]
+
+    if (
+        any(s not in cur_allowed for s in textbox.text())
+        or textbox.text().count(".") > 1
+    ):
+        textbox.setText(textbox.text()[:-1])
+
 
 def main():
     app = QApplication(sys.argv)
     window = uic.loadUi("MainWindow.ui")
     window.setFixedSize(467, 396)
     textbox = window.lineEdit
+    textbox.textChanged.connect(lambda: check_input(textbox))
     window.pushButton0.clicked.connect(lambda: append_to_textbox(textbox, 0))
     window.pushButton1.clicked.connect(lambda: append_to_textbox(textbox, 1))
     window.pushButton2.clicked.connect(lambda: append_to_textbox(textbox, 2))
@@ -58,6 +83,7 @@ def main():
     window.pushButton7.clicked.connect(lambda: append_to_textbox(textbox, 7))
     window.pushButton8.clicked.connect(lambda: append_to_textbox(textbox, 8))
     window.pushButton9.clicked.connect(lambda: append_to_textbox(textbox, 9))
+    window.pushButtonDot.clicked.connect(lambda: append_to_textbox(textbox, "."))
     window.pushButton_change_mode.clicked.connect(lambda: change_mode(window))
     window.pushButton_convert.clicked.connect(lambda: convert(textbox, window))
     window.menu.addAction("Clear text", lambda: clear_textbox(textbox))
--- lab1_2/utils.py
+++ lab1_2/utils.py
@@ -1,4 +1,7 @@
-def convert(number, from_base, to_base):
+EPS = 1e-4
+
+
+def convert_int(number, from_base, to_base):
     if not isinstance(number, int):
         number = int(number, from_base)
     alph = "0123456789ABCDEF"
@@ -9,3 +12,42 @@ def convert(number, from_base, to_base):
 
     return new_num[::-1]
 
+
+def convert_float(number, from_base, to_base, prec):
+    if not isinstance(number, float):
+        number = __convert_float(number, from_base)
+    alph = "0123456789ABCDEF"
+    new_num = ""
+
+    integer_part = int(number)
+    decimal_part = number % 1
+
+    # Conevrting int part
+    while integer_part > 0:
+        new_num += alph[integer_part % to_base]
+        integer_part //= to_base
+
+    new_num = new_num[::-1]
+    new_num += "."
+
+    # Converting float part
+    i = 0
+    while i < prec and abs(decimal_part) > EPS:
+        i += 1
+        decimal_part *= to_base
+        new_num += alph[int(decimal_part)]
+
+        if decimal_part >= 1:
+            decimal_part %= 1
+
+    return new_num
+
+
+def __convert_float(number, from_base):
+    int_part, float_part = number.split(".")
+    number = int(int_part, from_base)
+
+    for i, el in enumerate(float_part):
+        number += int(el) * from_base ** (-i - 1)
+
+    return number
